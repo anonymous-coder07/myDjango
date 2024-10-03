c548e91e7069d5d0ac8984915ba530dc91213691
M. Prathamesh Shivanand <18008@apsbolarum.edu.in>
2024-10-03 15:20:03 +0530

    deleted polls app

--- myDjango/djangoProject/polls/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse('Hello World, You are at Polls Index')
-
-
-# Create your views here.
