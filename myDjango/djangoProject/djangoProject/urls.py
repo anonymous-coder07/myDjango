07c025890382ed81a7f9bdfaab240e341dda4aec
M. Prathamesh Shivanand <18008@apsbolarum.edu.in>
2024-10-03 15:48:05 +0530

    changes made

--- myDjango/djangoProject/djangoProject/urls.py
+++ myDjango/djangoProject/djangoProject/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
+    path('hello/', include('hello.urls')),
     path('admin/', admin.site.urls),
 ]
 
